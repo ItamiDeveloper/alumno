8bd79fcc9471da91ffd8230f5dc8d38a4b144921
ItamiDeveloper <carlosedu062010@hotmail.com>
2024-11-27 22:53:31 -0500

    first commit

---
+++ src/main/java/com/example/alumno/model/User.java
@@ -0,0 +1,79 @@
+package com.example.alumno.model;
+
+import java.time.LocalDate;
+
+public class User {
+    private String name;
+    private String lastNameFather;
+    private String lastNameMother;
+    private LocalDate birthDate;
+    private String placeOfResidence;
+    private int age;
+    private String phone;
+    private String email;
+
+    // Getters y setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastNameFather() {
+        return lastNameFather;
+    }
+
+    public void setLastNameFather(String lastNameFather) {
+        this.lastNameFather = lastNameFather;
+    }
+
+    public String getLastNameMother() {
+        return lastNameMother;
+    }
+
+    public void setLastNameMother(String lastNameMother) {
+        this.lastNameMother = lastNameMother;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getPlaceOfResidence() {
+        return placeOfResidence;
+    }
+
+    public void setPlaceOfResidence(String placeOfResidence) {
+        this.placeOfResidence = placeOfResidence;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
